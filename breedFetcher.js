40d2a91eb973868e2e17053f51557d32950cf0d1
ArvinRad <radanarvin@gmail.com>
2021-04-08 21:49:07 +0000

    Finding a cat breed data from its API

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,13 +2,14 @@ const address = process.argv;
 const request = require('request');
 let breed = address[2];
 let data = {};
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log(`${error} - the requested breed is not found.`);
-  }
-  console.log(data);
-});
 
-
-
+const  fetchBreedDescription = () => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log(`${error} - the requested breed is not found.`);
+    }
+    return console.log(data);
+  });
+};
+fetchBreedDescription(breed);
